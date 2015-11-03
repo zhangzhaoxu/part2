0dd3309ec6be996c07bc668d18c4f3a766cd5e9d
zhangzhaoxu <755760826@qq.com>
2015-11-03 10:55:45 +0800

    暂时先用着版本

--- public/js/index.js
+++ public/js/index.js
@@ -29,6 +29,7 @@ function DialogController($scope,$mdDialog,$http){
 
     $scope.Daychange = function(event, date) {
         event.preventDefault();
+        $scope.select=1;
         var TemporaryDate = date._d.getFullYear()+"/"+(date._d.getMonth()+1)+"/"+date._d.getDate();
         if(date.selected){
             angular.forEach($scope.DayCourse, function (course, index) {
@@ -45,25 +46,30 @@ function DialogController($scope,$mdDialog,$http){
                 num:$scope.select
             });
         }
-        $scope.select=1;
     };
 
-    $scope.MonthChanged = function(newMonth, oldMonth){   //当月份发生变化时，更换$scope.DayCourse的值。
-
-        $scope.DayCourse = [];
-
-        //$http({
-        //    method:"GET",
-        //    url:"",
-        //    data:{
-        //        newMonth:newMonth.format('YYYY-M-DD'),
-        //        oldMonth:oldMonth.format('YYYY-M-DD')||null
-        //    }
-        //}).success(function(data){
-        //    $scope.DayCourse = data||[];
-        //}).error(function(err){
-        //    alert("err");
-        //});
+    $scope.changeDayCourseNum = function () {
+        if($scope.DayCourse.length!=0){
+            $scope.DayCourse[$scope.DayCourse.length-1].num = $scope.select;
+        }
     };
-    $scope.MonthChanged(new Date().getMonth()+1,null); //初次登录时，加载该月份DayCourse
+
+    //$scope.MonthChanged = function(newMonth, oldMonth){   //当月份发生变化时，更换$scope.DayCourse的值。
+    //
+    //    $scope.DayCourse = [];
+    //
+    //    //$http({
+    //    //    method:"GET",
+    //    //    url:"",
+    //    //    data:{
+    //    //        newMonth:newMonth.format('YYYY-M-DD'),
+    //    //        oldMonth:oldMonth.format('YYYY-M-DD')||null
+    //    //    }
+    //    //}).success(function(data){
+    //    //    $scope.DayCourse = data||[];
+    //    //}).error(function(err){
+    //    //    alert("err");
+    //    //});
+    //};
+    //$scope.MonthChanged(new Date().getMonth()+1,null); //初次登录时，加载该月份DayCourse
 }
